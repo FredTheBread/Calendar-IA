c1566211309aa4c745f5d815c2f1193d00855ab3
Michael Cole <michaelc2023@student.vis.ac.at>
2023-01-27 19:17:22 +0100

    Second Commit - Added ViewController, Month changes and correct dates

--- Calendar/ViewController.swift
+++ Calendar/ViewController.swift
@@ -7,8 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     @IBOutlet weak var monthLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -19,19 +18,63 @@ class ViewController: UIViewController {
         
         super.viewDidLoad()
         setCellsView()
+        setMonthView()
     }
 
     func setCellsView() {
         let width = (collectionView.frame.size.width - 2) / 8
         let height = (collectionView.frame.size.height - 2) / 8
     
-        let flowLayout = collectionView
+        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.itemSize = CGSize(width: width, height: height)
+    }
+    
+    func setMonthView() {
+        totalSquares.removeAll()
+        
+        let daysInMonth = CalendarHelper().daysInMonth(date: selectedDate)
+        let firstDayOfMonth = CalendarHelper().firstOfMonth(date: selectedDate)
+        let startingSpaces = CalendarHelper().weekDay(date: firstDayOfMonth)
+        
+        var count: Int = 1 //2 for mon start of the week instea of sun
+        
+        while(count <= 42) {
+            if(count <= startingSpaces || count - startingSpaces > daysInMonth){
+                totalSquares.append("")
+            }
+            else {
+                totalSquares.append(String(count - startingSpaces))
+            }
+            count += 1
+        }
+        monthLabel.text = CalendarHelper().monthString(date: selectedDate) + " " + CalendarHelper().yearString(date: selectedDate)
+        collectionView.reloadData()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CalendarCell
+        
+        cell.dayOfMonth.text = totalSquares[indexPath.item]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        totalSquares.count
     }
     
     @IBAction func previousMonth(_ sender: Any) {
+        selectedDate = CalendarHelper().minusMonth(date: selectedDate)
+        setMonthView()
     }
     
     @IBAction func nextMonth(_ sender: Any) {
+        selectedDate = CalendarHelper().plusMonth(date: selectedDate)
+        setMonthView()
+    }
+    
+    override open var shouldAutorotate: Bool {
+        return false
     }
 }
 
