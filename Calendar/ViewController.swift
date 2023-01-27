1422222b76905950ebebe1a5f912f97da27e5500
Michael Cole <michaelc2023@student.vis.ac.at>
2023-01-27 18:00:21 +0100

    First commit - Project Creation and initial setup

---
+++ Calendar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calendar
+//
+//  Created by Michael Cole on 27.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  Calendar
+//
+//  Created by Michael Cole on 27.01.23.
+//
+
+import Foundation
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    
+}
